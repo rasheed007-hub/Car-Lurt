5803195c63aa8271a5803eba632fb03f6e64c5f0
rasheed <rasheedoduola007@gamil.com>
2025-03-28 12:01:24 +0100

    ``` Add new features and improve styling for better user experience ```

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -26,16 +26,14 @@ const ShopContextProvider = (props) => {
      
     const getTotalCartAmount = () => {
         let totalAmount = 0;
-        for(const item in cartItems)
-        {
-            if(cartItems[item]>0)
-            {
-                let itemInfo = all_product.find((product) => product.id === Number(item))
-                totalAmount += itemInfo.new_price * cartItems[item];                               
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = all_product.find((product) => product.id === Number(item));
+                totalAmount += itemInfo.new_price * cartItems[item];
             }
-            return totalAmount;
         }
-    }
+        return totalAmount; // Move this outside the loop
+    };
 
     const getTotalCartItems = () => {
         let totalItem = 0 ;
